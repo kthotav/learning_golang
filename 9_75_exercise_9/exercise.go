0db05562995e6ee742a1ea6fb6701743c5e6b662
Karthik <kthotav@gmail.com>
2020-03-23 22:35:14 -0700

    adding exercise of switch statement

---
+++ 9_75_exercise_9/exercise.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	favSport := "basketball"
+
+	switch favSport {
+	case "soccoer":
+		fmt.Println("Soccer is my favorite sport!")
+	case "tennis":
+		fmt.Println("Tennis is my favorite sport!")
+	case "basketball":
+		fmt.Println("Basketball is my favorite sport!")
+	}
+}
